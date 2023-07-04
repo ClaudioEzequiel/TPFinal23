f042ee92db461f79f17309c261b752fb88074bd4
ClaudioEzequiel <guzmanclaudio.ezequiel@gmail.com>
2023-07-03 21:27:09 -0300

    probando back de fabian

--- js/producto-edit.js
+++ js/producto-edit.js
@@ -11,7 +11,7 @@ createApp({
       imagen: "",
       stock: 0,
       precio: 0,
-      url: "http://claudioezequiel.pythonanywhere.com/productos" + id,
+      url: "https://suarezfabian.pythonanywhere.com/productos" + id,
     };
   },
   methods: {
