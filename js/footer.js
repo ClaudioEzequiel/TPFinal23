ec7464085f9cc8ec8810967fc93d4265a4026a5c
Fabian <129339361+suarezfabian@users.noreply.github.com>
2023-07-04 21:20:40 -0300

    se agrego el archivo footer.js
    es el footer del sitio

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+/* Este código selecciona el elemento del documento con el id "header" y le asigna un bloque de código HTML para construir una barra de navegación. El código HTML contiene una estructura de navegación típica de una barra de navegación de Bootstrap, con enlaces a diferentes páginas y un formulario de búsqueda.
+El código se encarga de asignar el contenido HTML al elemento con el id "header", lo que resulta en la visualización de la barra de navegación en ese lugar dentro del documento HTML.
+*/
+document.getElementById(
+  "footer"
+).innerHTML = `
+<div class="row">
+		<div class="col-md-12 copy">
+			<p class="text-center">&copy; Copyright 2023 - Tienda TECNO.  All rights reserved.</p>
+			<p class="text-center">Diseño realizado por Claudio Guzman | Fabián Suárez.</p>
+		</div>
+	</div>
+`;
