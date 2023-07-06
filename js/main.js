cd41d229dd84e459734575242f80d0f54b86e3a2
Fabian <129339361+suarezfabian@users.noreply.github.com>
2023-07-06 17:17:42 -0300

    Update main.js
    se volvio al original el codigo de "Buscar"

--- js/main.js
+++ js/main.js
@@ -42,8 +42,9 @@ document.getElementById(
       </ul>
       <form class="d-flex my-2 my-lg-0">
           <input class="form-control me-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
       </form>
+
   </div>
 </div>
 </nav>
